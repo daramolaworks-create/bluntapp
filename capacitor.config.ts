4c53a6ae5b470af673794adcde01db70a479f02f
Daramola Ben <wudan@Daramolas-MacBook-Pro.local>
2026-04-10 12:46:31 +0100

    chore: removed big b logo splash screen; replaced with blank dark overlay

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -9,7 +9,7 @@ const config: CapacitorConfig = {
   },
   plugins: {
     SplashScreen: {
-      launchShowDuration: 2000,
+      launchShowDuration: 0,
       launchAutoHide: true,
       backgroundColor: '#0a1128',
       showSpinner: false,
